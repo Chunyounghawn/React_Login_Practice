0756f8dfb91df07c2703fabf3f70269bd420a085
Chunyounghawn <zing0982@naver.com>
2022-07-19 16:25:01 +0900

    .

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:'mongodb+srv://younghawn:5252@reactfirstcluster.ydvor.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURI: process.env.MONGO_URI
     //mongoURI: process.env.MONGO_URI //헤로쿠이용하면 씀.
 }
